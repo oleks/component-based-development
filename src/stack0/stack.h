0723ecad04924e596f683dbd63166d3d38d31ed4
Oleksandr Shturmov <oleks@oleks.info>
2016-03-27 14:38:39 +0200

    shorter docs

--- src/stack0/stack.h
+++ src/stack0/stack.h
@@ -4,11 +4,11 @@
 #define STACK_OVERFLOW  (-1)
 #define STACK_UNDERFLOW (-2)
 
-// @return 0 on success, or STACK_OVERFLOW if stack is full.
+// @return 0 on success; STACK_OVERFLOW if stack is full.
 int
 stack_push(int value);
 
-// @return 0 on success, or STACK_UNDERFLOW if stack is empty.
+// @return 0 on success; STACK_UNDERFLOW if stack is empty.
 int
 stack_pop(int *value);
 
