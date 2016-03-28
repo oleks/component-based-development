95f2b2b9bc82b70c04ee0428b4509585b29a77f7
Oleksandr Shturmov <oleks@oleks.info>
2016-03-28 11:55:56 +0200

    run-time size stack

---
+++ src/stack1/stack.c
@@ -0,0 +1,28 @@
+#include "stack.h"
+
+void
+stack_init(struct stack *stack, int *values, size_t size) {
+  stack->values = values;
+  stack->count = 0;
+  stack->size = size;
+}
+
+int
+stack_push(struct stack *stack, int value) {
+  if (stack->count == stack->size) {
+    return STACK_OVERFLOW;
+  }
+
+  stack->values[stack->count++] = value;
+  return 0;
+}
+
+int
+stack_pop(struct stack *stack, int *value) {
+  if (stack->count == 0) {
+    return STACK_UNDERFLOW;
+  }
+
+  *value = stack->values[stack->count--];
+  return 0;
+}
---
+++ src/stack1/stack.h
@@ -0,0 +1,34 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stddef.h> // size_t
+
+#define STACK_OVERFLOW  (-1)
+#define STACK_UNDERFLOW (-2)
+
+struct stack {
+  int *values;
+  size_t count;
+  size_t size;
+};
+
+// Should be called before all other stack-related functions with the pointer
+// that later will be used in stack-related functions.
+//
+// @param stack Assumed to point to a valid stack.
+// @param values Assumed to point to an array.
+void
+stack_init(struct stack *stack, int *values, size_t size);
+
+// @param stack Assumed to point to valid stack.
+// @return 0 on success; STACK_OVERFLOW if stack is full.
+int
+stack_push(struct stack *stack, int value);
+
+// @param stack Assumed to point to a valid stack.
+// @param value Assumed to point to memory we can write to.
+// @return 0 on success; STACK_UNDERFLOW if stack is empty.
+int
+stack_pop(struct stack *stack, int *value);
+
+#endif // STACK_H
